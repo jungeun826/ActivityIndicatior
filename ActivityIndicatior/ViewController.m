53f38c418c2a5289a2ef72a7c6e58cc6398191c0
jungeun826 <jungeun826@nate.com>
2014-01-03 15:42:59 +0900

    Activate Indicator 실습 2.10.1을 완료했다.

--- ActivityIndicatior/ViewController.m
+++ ActivityIndicatior/ViewController.m
@@ -9,15 +9,30 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *indicatiorIB;
+@property (strong, nonatomic) UIActivityIndicatorView *indicatorCode;
 
 @end
 
 @implementation ViewController
+- (IBAction)startIndicator:(id)sender {
+    [self.indicatiorIB startAnimating];
+    [self.indicatorCode performSelector:@selector(startAnimating) withObject:nil afterDelay:0.3];
+}
+- (IBAction)stopIndicator:(id)sender {
+    [self.indicatiorIB stopAnimating];
+    [self.indicatorCode performSelector:@selector(stopAnimating) withObject:nil afterDelay:0.7];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.indicatorCode = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.indicatorCode.hidesWhenStopped = YES;
+    self.indicatorCode.center = CGPointMake(160, 270);
+    
+    [self.view addSubview:self.indicatorCode];
 }
 
 - (void)didReceiveMemoryWarning
